1ef6979e8f79d351b1c45bf0e230455aa87833de
inchoxd <rjb2066@gmail.com>
2022-02-03 03:04:40 +0900

    cliパッケージ化, jwcryptoのバージョン指定。

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from distutils.core import setup
 from setuptools import setup, find_packages
-setup(packages=find_packages())
+setup(
+        install_requires=[
+            "jwcrypto>=1.0"
+            ],
+        entry_points={
+            "console_scripts":[
+                "genjwtkey=genjwtkey.generate:generate"
+                ]
+            },
+        packages=find_packages()
+        )
+
