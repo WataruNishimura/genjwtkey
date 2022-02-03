3e0031d383968928e2f99ca6e6a39140dae252c8
WataruNishimura <wataru.nishimura@kraftsman.jp>
2022-02-03 17:43:27 +0900

    cliパッケージをsetup.cfgに記載

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
 setup(
-        install_requires=[
-            "jwcrypto>=1.0"
-            ],
-        entry_points={
-            "console_scripts":[
-                "genjwtkey=genjwtkey.generate:generate"
-                ]
-            },
-        packages=find_packages()
-        )
+    packages=find_packages()
+)
 
